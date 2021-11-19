8a9de84ede3c2792f0d4aabe186999f1c27a7c02
Lucio Baglione <coire1@gmail.com>
2021-11-19 11:01:34 +0100

    Fix wording.

--- src/locales/messages/en.js
+++ src/locales/messages/en.js
@@ -15,7 +15,7 @@ export default {
     SELECTED_GOALS: "Selected goals (refine them before the next step)",
     SELECT_SUBGOAL: "Choose a sub-goal from the list (the sub-goals listed are based on your SDG selection in the previous step):",
     SELECTED_SUBGOALS: "Selected sub-goals (refine them before the next step)",
-    SELECT_METRICS: "<p>You can find below the measurements metrics related to the <b>SDG sub-goals</b> you selected: choose the metrics you want to include in your proposal or add your own matrics.</p><p>We recommend also adding your specific <b>Key Performance Indicator (KPI)</b> for each measurement to increase the audibility of the proposal.</p>",
+    SELECT_METRICS: "<p>You can find below the measurements metrics related to the <b>SDG sub-goals</b> you selected: choose the metrics you want to include in your proposal and copy-paste into IdeaScale for further edit.</p><p>We recommend also adding your specific <b>Key Performance Indicator (KPI)</b> for each measurement to increase the audibility of the proposal.</p>",
     SELECTED_METRICS: "Selected metrics and summary",
     SELECTED_METRICS_MSG: "",
   }
